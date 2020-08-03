b049199c6cd9a89da4b06b87fb443f366a1cf199
Jay Moon <sjaymoon15@gmail.com>
2020-08-03 12:49:35 -0500

    Create ListBooks component and fetch bookshelf data.

--- src/App.js
+++ src/App.js
@@ -6,12 +6,26 @@ import ListBooks from './ListBooks';
 import Search from './Search';
 
 class BooksApp extends React.Component {
-  state = {};
+  state = {
+    books: [],
+  };
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState(() => ({
+        books,
+      }));
+    });
+  }
 
   render() {
     return (
       <div>
-        <Route exact path='/' render={() => <ListBooks />} />
+        <Route
+          exact
+          path='/'
+          render={() => <ListBooks books={this.state.books} />}
+        />
         <Route path='/search' render={() => <Search />} />
       </div>
     );
