9f1a629a68cfeffae0e105084b66c78f97d071e3
Jay Moon <sjaymoon15@gmail.com>
2020-08-05 10:27:47 -0500

    Change Stateless Class Component into Function Component.

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,34 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Book from './Book';
 
-class BookShelf extends Component {
-  extractBookShelfName(shelfValue) {
-    if (shelfValue === 'currentlyReading') {
-      return 'Currently Reading';
-    } else if (shelfValue === 'wantToRead') {
-      return 'Want To Read';
-    } else if (shelfValue === 'read') {
-      return 'Read';
-    }
-  }
-  render() {
-    const { bookShelfBooks, bookShelfNameKey, onSelectShelf } = this.props;
-    const bookShelfName = this.extractBookShelfName(bookShelfNameKey);
-    return (
-      <div className='bookshelf'>
-        <h2 className='bookshelf-title'>{bookShelfName}</h2>
-        <div className='bookshelf-books'>
-          <ol className='books-grid'>
-            {bookShelfBooks.map((book) => {
-              return (
-                <Book book={book} onSelectShelf={onSelectShelf} key={book.id} />
-              );
-            })}
-          </ol>
-        </div>
+const BookShelf = ({ bookShelfBooks, bookShelfNameKey, onSelectShelf }) => {
+  const bookShelfName = extractBookShelfName(bookShelfNameKey);
+  return (
+    <div className='bookshelf'>
+      <h2 className='bookshelf-title'>{bookShelfName}</h2>
+      <div className='bookshelf-books'>
+        <ol className='books-grid'>
+          {bookShelfBooks.map((book) => {
+            return (
+              <Book book={book} onSelectShelf={onSelectShelf} key={book.id} />
+            );
+          })}
+        </ol>
       </div>
-    );
+    </div>
+  );
+};
+
+const extractBookShelfName = (shelfValue) => {
+  if (shelfValue === 'currentlyReading') {
+    return 'Currently Reading';
+  } else if (shelfValue === 'wantToRead') {
+    return 'Want To Read';
+  } else if (shelfValue === 'read') {
+    return 'Read';
   }
-}
+};
 
 export default BookShelf;
